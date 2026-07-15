ef908e0ffeb44c63ad46144bae99119241ec3199
Rahulbiradar9 <rahulbiradar7134@gmail.com>
2026-07-15 09:50:15 +0530

    Solved .git

--- push.bat
+++ push.bat
@@ -1,58 +1,23 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-REM ==========================================
-REM Start Timer
-REM ==========================================
-for /f %%i in ('powershell -NoProfile -Command "[DateTime]::UtcNow.Ticks"') do set START=%%i
-
-REM ==========================================
-REM Find latest CPP file
-REM ==========================================
-set "FILE="
-
-for /f "delims=" %%f in ('dir /b /a-d /o-d *.cpp') do (
-    set "FILE=%%~nf"
+REM Find the most recently modified folder
+for /f "delims=" %%d in ('dir /ad /b /o-d') do (
+    set "LATEST=%%d"
     goto :found
 )
 
 :found
-
-if not defined FILE (
-    echo No .cpp files found.
+if not defined LATEST (
+    echo No problem folders found.
     pause
     exit /b
 )
 
-echo Latest Problem: !FILE!
-
-REM Current Date & Time
-for /f "delims=" %%i in ('powershell -NoProfile -Command "Get-Date -Format ''yyyy-MM-dd HH:mm:ss''"') do set DATETIME=%%i
+echo Latest problem: !LATEST!
 
 git add .
-
-git commit -m "Solved !FILE! | !DATETIME!"
-
-if errorlevel 1 (
-    echo Nothing to commit.
-    pause
-    exit /b
-)
-
+git commit -m "Solved !LATEST!"
 git push origin main
-
-REM ==========================================
-REM Stop Timer
-REM ==========================================
-for /f %%i in ('powershell -NoProfile -Command "[DateTime]::UtcNow.Ticks"') do set END=%%i
-
-for /f %%i in ('powershell -NoProfile -Command "([TimeSpan]::FromTicks(%END%-%START%)).TotalMilliseconds.ToString('F0')"') do set MS=%%i
-
-echo.
-echo ===================================
-echo Problem : !FILE!
-echo Date    : !DATETIME!
-echo Time    : !MS! ms
-echo ===================================
 
 pause
