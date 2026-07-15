220395ccc45a8f7cbf12e54b783846d81be670db
Rahulbiradar9 <rahulbiradar7134@gmail.com>
2026-07-15 09:46:50 +0530

    Solved .git | 2026-07-15

--- push.bat
+++ push.bat
@@ -1,6 +1,11 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+REM ----------------------------
+REM Start Timer
+REM ----------------------------
+powershell -Command "$start = Get-Date"
+
 REM Find the most recently modified folder
 for /f "delims=" %%d in ('dir /ad /b /o-d') do (
     set "LATEST=%%d"
@@ -16,8 +21,17 @@ if not defined LATEST (
 
 echo Latest problem: !LATEST!
 
+REM Current Date & Time
+for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format \"yyyy-MM-dd HH:mm:ss\""') do set DATETIME=%%i
+
 git add .
-git commit -m "Solved !LATEST!"
+git commit -m "Solved !LATEST! | %DATETIME%"
 git push origin main
+
+REM ----------------------------
+REM End Timer
+REM ----------------------------
+powershell -NoProfile -Command ^
+"$elapsed=((Get-Date)-([datetime]'%DATE% %TIME%')).TotalMilliseconds; Write-Host ('Execution Time: {0:N0} ms' -f $elapsed)"
 
 pause
